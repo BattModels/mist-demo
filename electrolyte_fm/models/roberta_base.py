fd6e45891d7df9fe92b0ecdeb43a7a4a21ca916e
Alexius Wadell <awadell@gmail.com>
2024-03-11 12:07:29 -0400

    feat: expose more model size hyperparameters

--- electrolyte_fm/models/roberta_base.py
+++ electrolyte_fm/models/roberta_base.py
@@ -11,19 +11,27 @@ class RoBERTa(pl.LightningModule):
     def __init__(
         self,
         vocab_size: int,
+        intermediate_size: int = 3072,
         max_position_embeddings: int = 512,
         num_attention_heads: int = 12,
         num_hidden_layers: int = 6,
+        hidden_size: int = 768,
+        learning_rate: float = 1.6e-4,
     ) -> None:
         super().__init__()
+        self.learning_rate = learning_rate
         self.vocab_size = vocab_size
         self.save_hyperparameters()
 
         self.config = RobertaConfig(
             vocab_size=vocab_size,
+            intermediate_size=intermediate_size,
+            hidden_size=hidden_size,
             max_position_embeddings=max_position_embeddings,
             num_attention_heads=num_attention_heads,
             num_hidden_layers=num_hidden_layers,
+            hidden_dropout_prob=0.1,
+            attention_probs_dropout_prob=0.1,
             type_vocab_size=1,
         )
         self.model = RobertaForMaskedLM(config=self.config)
@@ -110,6 +118,7 @@ class RoBERTa(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(
-            [p for n, p in self.model.named_parameters()], lr=1e-4
+            [p for _, p in self.model.named_parameters()],
+            lr=self.learning_rate,
         )
         return optimizer
