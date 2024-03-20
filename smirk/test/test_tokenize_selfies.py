680a8ba150b3a5e1be79b94047b88ef4289884be
Anoushka Bhutani <abhutani@polaris-login-01.hsn.cm.polaris.alcf.anl.gov>
2024-03-20 18:37:08 +0000

    fix: tokenization pytest

--- smirk/test/test_tokenize_selfies.py
+++ smirk/test/test_tokenize_selfies.py
@@ -1,37 +1,45 @@
-import smirk
-from pathlib import Path
-
-VOCAB_FILE = Path(__file__).parent.parent.joinpath("vocab_selfies.json").resolve()
-assert VOCAB_FILE.is_file()
-
-
-def test_smirk():
-    tok = smirk.SmirkTokenizer(str(VOCAB_FILE), is_smiles=False)
-    smile = "[C][N][=C][=O]"
-    emb = tok.encode(smile)
-    print(emb)
-    assert len(emb.ids) == 10
-    smile_out = tok.decode(emb.ids)
-    print(smile_out)
-    assert smile_out == smile
+from smirk.smirk import SmirkTokenizer
+import pytest
+import json
+from copy import deepcopy
+from importlib.resources import files
 
 
-def test_encode_batch():
-    tok = smirk.SmirkTokenizer(str(VOCAB_FILE), is_smiles=False)
-    smile = [
+@pytest.fixture
+def tokenizer():
+    VOCAB_FILE = files("smirk").joinpath("vocab_selfies.json")
+    assert VOCAB_FILE.is_file()
+    return SmirkTokenizer(str(VOCAB_FILE), is_smiles=False)
+
+
+@pytest.fixture
+def selfie_strings():
+    return [
+        "[C][N][=C][=O]",
         "[C][N][C][=N][C][=C][Ring1][Branch1][C][=Branch1][C][=O][N][Branch1][=Branch2][C][=Branch1][C][=O][N][Ring1][Branch2][C][C]",
-        "[C][C][N][Branch1][Ring1][C][C][C][=Branch1][C][=O][C@H1][C][N][Branch2][Ring1][Branch2][C@@H1][C][C][=C][NH1][C][=C][Ring1][Branch1][C][=Branch1][=Branch1][=C][C][=C][Ring1][=Branch1][C][Ring1][N][=C][Ring1][S][C]""",
     ]
-    emb = tok.encode_batch(smile)
-    assert len(emb) == 2
-    assert len(emb[0].ids) == 63
-    assert len(emb[1].ids) == 114  # `@@` is one token
-    assert len(emb[0].ids) == len(emb[0].attention_mask)
-    assert len(emb[0].ids) == len(emb[0].type_ids)
-    smile_out = tok.decode_batch([e.ids for e in emb])
-    assert len(smile_out) == len(smile)
-    assert smile_out == smile
-    assert smile_out[0] == smile[0]
 
-if __name__=="__main__":
-    test_smirk()
+
+def check_encode(tok, x):
+    img = tok.decode(tok.encode(x)["input_ids"])
+    assert img == x
+
+
+def test_pretokenize(tokenizer):
+    splits = tokenizer.pretokenize("[C][N][=C][=O]")
+    assert splits == ["[", "C", "]", "[", "N", "]", "[", "=", "C", "]", "[", "=", "O", "]"]
+    assert len(splits) == 14
+
+
+def test_image(tokenizer, selfie_strings):
+    check_encode(tokenizer, "[C][N][=C][=O]")
+    assert len(tokenizer.encode("[C][N][=C][=O]")["input_ids"]) == 14
+    for x in selfie_strings:
+        check_encode(tokenizer, x)
+
+
+def test_encode(tokenizer):
+    selfie = "[C][N][C][=N][C][=C][Ring1][Branch1][C][=Branch1][C][=O][N][Branch1][=Branch2][C][=Branch1][C][=O][N][Ring1][Branch2][C][C]"
+    emb = tokenizer.encode(selfie)
+    out = tokenizer.decode(emb["input_ids"])
+    assert out == selfie
--- smirk/test/test_tokenize_smiles.py
+++ smirk/test/test_tokenize_smiles.py
@@ -1,33 +1,76 @@
-import smirk
-from pathlib import Path
-
-VOCAB_FILE = Path(__file__).parent.parent.joinpath("vocab_smiles.json").resolve()
-assert VOCAB_FILE.is_file()
-
-
-def test_smirk():
-    tok = smirk.SmirkTokenizer(str(VOCAB_FILE), is_smiles=True)
-    smile = "COCCC(=O)N1CCN(C)C(C2=CN([C@@H](C)C3=CC=C(C(F)(F)F)C=C3)N=N2)C1"
-    emb = tok.encode(smile)
-    assert len(emb.ids) == 60
-    smile_out = tok.decode(emb.ids)
-    print(smile_out)
-    assert smile_out == smile
+from smirk.smirk import SmirkTokenizer
+import pytest
+import json
+from copy import deepcopy
+from importlib.resources import files
 
 
-def test_encode_batch():
-    tok = smirk.SmirkTokenizer(str(VOCAB_FILE), is_smiles=True)
-    smile = [
+@pytest.fixture
+def tokenizer():
+    VOCAB_FILE = files("smirk").joinpath("vocab_smiles.json")
+    assert VOCAB_FILE.is_file()
+    return SmirkTokenizer(str(VOCAB_FILE), is_smiles=True)
+
+
+@pytest.fixture
+def smile_strings():
+    return [
         "CN1C=NC2=C1C(=O)N(C(=O)N2C)C",
         "CCN(CC)C(=O)[C@H]1CN([C@@H]2Cc3c[nH]c4c3c(ccc4)C2=C1)C",
     ]
-    emb = tok.encode_batch(smile)
-    assert len(emb) == 2
-    assert len(emb[0].ids) == 27
-    assert len(emb[1].ids) == 53  # `@@` is one token
-    assert len(emb[0].ids) == len(emb[0].attention_mask)
-    assert len(emb[0].ids) == len(emb[0].type_ids)
-    smile_out = tok.decode_batch([e.ids for e in emb])
-    assert len(smile_out) == len(smile)
+
+
+def check_encode(tok, x):
+    img = tok.decode(tok.encode(x)["input_ids"])
+    assert img == x
+
+
+def test_pretokenize(tokenizer):
+    splits = tokenizer.pretokenize("OC[C@@H][OH]")
+    assert splits == ["O", "C", "[", "C", "@@", "H", "]", "[", "O", "H", "]"]
+    assert len(splits) == 11
+
+
+def test_image(tokenizer, smile_strings):
+    check_encode(tokenizer, "OC[C@@H][OH]")
+    assert len(tokenizer.encode("OC[C@@H][OH]")["input_ids"]) == 11
+    for x in smile_strings:
+        check_encode(tokenizer, x)
+
+
+def test_encode(tokenizer, smile_strings):
+    smile = "COCCC(=O)N1CCN(C)C(C2=CN([C@@H](C)C3=CC=C(C(F)(F)F)C=C3)N=N2)C1"
+    emb = tokenizer.encode(smile)
+    smile_out = tokenizer.decode(emb["input_ids"])
     assert smile_out == smile
-    assert smile_out[0] == smile[0]
+
+
+def test_encode_batch(tokenizer, smile_strings):
+    emb = tokenizer.encode_batch(smile_strings)
+    assert len(emb) == 2
+    assert len(emb[0]["input_ids"]) == len(emb[0]["attention_mask"])
+    assert len(emb[0]["input_ids"]) == len(emb[0]["token_type_ids"])
+    assert len(emb[0]["input_ids"]) == len(emb[0]["special_tokens_mask"])
+    smile_out = tokenizer.decode_batch([e["input_ids"] for e in emb])
+    assert len(smile_out) == len(smile_strings)
+    assert smile_out == smile_strings
+    assert smile_out[0] == smile_strings[0]
+    assert smile_out[1] == smile_strings[1]
+
+
+def test_serialize(tokenizer):
+    tok = deepcopy(tokenizer)
+    config = json.loads(tok.to_str())
+    assert config["decoder"] == {"type": "Fuse"}
+    assert config["model"]["type"] == "WordLevel"
+    assert config["pre_tokenizer"] == {"type": "SmirkPreTokenizer"}
+
+
+def test_special(tokenizer):
+    assert tokenizer.special_tokens["bos_token"] == "[BOS]"
+    assert tokenizer.special_tokens["eos_token"] == "[EOS]"
+    assert tokenizer.special_tokens["pad_token"] == "[PAD]"
+    assert tokenizer.special_tokens["sep_token"] == "[SEP]"
+    assert tokenizer.special_tokens["cls_token"] == "[CLS]"
+    assert tokenizer.special_tokens["mask_token"] == "[MASK]"
+    assert tokenizer.special_tokens["unk_token"] == "[UNK]"
