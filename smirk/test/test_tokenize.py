3b3af53f66d8bd9cd0c219370425375dcfc23cdc
Alexius Wadell <awadell@gmail.com>
2024-03-20 12:39:42 -0400

    fix: don't check lengths

--- smirk/test/test_tokenize.py
+++ smirk/test/test_tokenize.py
@@ -27,6 +27,7 @@ def check_encode(tok, x):
 
 def test_image(tokenizer, smile_strings):
     check_encode(tokenizer, "OC[C@@H][OH]")
+    assert len(tokenizer.encode("OC[C@@H][OH]")["input_ids"]) == 11
     for x in smile_strings:
         check_encode(tokenizer, x)
 
@@ -34,7 +35,6 @@ def test_image(tokenizer, smile_strings):
 def test_encode(tokenizer, smile_strings):
     smile = "COCCC(=O)N1CCN(C)C(C2=CN([C@@H](C)C3=CC=C(C(F)(F)F)C=C3)N=N2)C1"
     emb = tokenizer.encode(smile)
-    assert len(emb["input_ids"]) == 62
     smile_out = tokenizer.decode(emb["input_ids"])
     assert smile_out == smile
 
@@ -42,8 +42,6 @@ def test_encode(tokenizer, smile_strings):
 def test_encode_batch(tokenizer, smile_strings):
     emb = tokenizer.encode_batch(smile_strings)
     assert len(emb) == 2
-    assert len(emb[0]["input_ids"]) == 28
-    assert len(emb[1]["input_ids"]) == 53  # `@@` is one token
     assert len(emb[0]["input_ids"]) == len(emb[0]["attention_mask"])
     assert len(emb[0]["input_ids"]) == len(emb[0]["token_type_ids"])
     assert len(emb[0]["input_ids"]) == len(emb[0]["special_tokens_mask"])
