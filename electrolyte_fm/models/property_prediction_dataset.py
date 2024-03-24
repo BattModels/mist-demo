42f5811a298cfb5b37748b18955dd1efa8a2a15c
anoushka2000 <abhutani@umich.edu>
2024-03-23 20:42:51 -0400

    fix: clean up PropertyPredictionDataset

--- electrolyte_fm/models/property_prediction_dataset.py
+++ electrolyte_fm/models/property_prediction_dataset.py
@@ -1,56 +1,30 @@
-import torch
 import os
-import pytorch_lightning as pl
+
 import numpy as np
 import pandas as pd
+import pytorch_lightning as pl
+import torch
 from torch.utils.data import DataLoader
+from transformers import AutoTokenizer, PreTrainedTokenizerBase
 from utils import normalize_smiles
-from transformers import (
-    AutoTokenizer,
-    PreTrainedTokenizerBase,
-)
+
 
 class PropertyPredictionDataset(torch.utils.data.Dataset):
-    def __init__(self, df,  measure_name, aug=True):
-        df = df.dropna()  # TODO - Check why some rows are na
-        self.df = df
-        all_smiles = df["smiles"].tolist()
-        self.original_smiles = []
-        self.original_canonical_map = {
-            smi: normalize_smiles(smi, canonical=True, isomeric=False) for smi in all_smiles
-        }
-        if measure_name:
-            all_measures = df[measure_name].tolist()
-            self.measure_map = {all_smiles[i]: all_measures[i] for i in range(len(all_smiles))}
-
-        # Get the canonical smiles
-        # Convert the keys to canonical smiles if not already
-
-        for i in range(len(all_smiles)):
-            smi = all_smiles[i]
-            if smi in self.original_canonical_map.keys():
-                self.original_smiles.append(smi)
-
-        print(f"Embeddings not found for {len(all_smiles) - len(self.original_smiles)} molecules")
-
-        self.aug = aug
-        self.is_measure_available = "measure" in df.columns
+    def __init__(self, df, measure_name):
+        df = df[['smiles', measure_name]]
+        df = df.dropna()
+        self.measure_name = measure_name
+        self.df = df.reset_index(drop=True)
 
     def __getitem__(self, index):
-        original_smiles = self.original_smiles[index]
-        canonical_smiles = self.original_canonical_map[original_smiles]
-        return canonical_smiles, self.measure_map[original_smiles]
-
+        return self.df.loc[index, 'smiles'], self.df.loc[index, self.measure_name]
+  
     def __len__(self):
-        return len(self.original_smiles)
+        return len(self.df)
 
 class PropertyPredictionDataModule(pl.LightningDataModule):
     def __init__(self, tokenizer, dataset_name):
         super().__init__()
-        # if type(hparams) is dict:
-        #     hparams = Namespace(**hparams)
-        # self.hparams = hparams
-        #self.smiles_emb_size = hparams.n_embd
         self.tokenizer: PreTrainedTokenizerBase = AutoTokenizer.from_pretrained(
             tokenizer,
             trust_remote_code=True,
@@ -77,36 +51,26 @@ class PropertyPredictionDataModule(pl.LightningDataModule):
             self.dataset_name, "test"
         )
 
-        train_ds = get_dataset(
-            self.hparams.data_root,
+        self.train_dataset = get_dataset(
+            self.path,
             train_filename,
-            self.hparams.train_dataset_length,
-            self.hparams.aug,
-            measure_name=self.hparams.measure_name,
+            self.train_dataset_length,
+            measure_name=self.measure_name,
         )
 
-        val_ds = get_dataset(
-            self.hparams.data_root,
+        self.val_dataset = get_dataset(
+            self.path,
             valid_filename,
-            self.hparams.eval_dataset_length,
-            aug=False,
-            measure_name=self.hparams.measure_name,
+            self.val_dataset_length,
+            measure_name=self.measure_name,
         )
 
-        test_ds = get_dataset(
+        self.test_dataset = get_dataset(
             self.hparams.data_root,
             test_filename,
-            self.hparams.eval_dataset_length,
-            aug=False,
-            measure_name=self.hparams.measure_name,
+            self.val_dataset_length,
+            measure_name=self.measure_name,
         )
-
-        self.train_ds = train_ds
-        self.val_ds = [val_ds] + [test_ds]
-
-        # print(
-        #     f"Train dataset size: {len(self.train_ds)}, val: {len(self.val_ds1), len(self.val_ds2)}, test: {len(self.test_ds)}"
-        # )
 
     def collate(self, batch):
         tokens = self.tokenizer.batch_encode_plus([ smile[0] for smile in batch], padding=True, add_special_tokens=True)
@@ -134,11 +98,11 @@ class PropertyPredictionDataModule(pl.LightningDataModule):
             collate_fn=self.collate,
         )
     
-def get_dataset(data_root, filename, dataset_len, aug, measure_name):
+def get_dataset(data_root, filename, dataset_len, measure_name):
     df = pd.read_csv(os.path.join(data_root, filename))
     print("Length of dataset:", len(df))
     if dataset_len:
         df = df.head(dataset_len)
         print("Warning entire dataset not used:", len(df))
-    dataset = PropertyPredictionDataset(df,  measure_name, aug)
+    dataset = PropertyPredictionDataset(df,  measure_name)
     return dataset
