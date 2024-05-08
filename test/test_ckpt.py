f74de1992dd763ff1ea2b426d6aa00b2a95ce53c
anoushka2000 <abhutani@umich.edu>
2024-05-08 11:44:31 -0400

    test for linked arguments in test_ckpt

--- test/test_ckpt.py
+++ test/test_ckpt.py
@@ -1,14 +1,16 @@
 import pytest
+from unittest import mock
+
 import json
 from pytorch_lightning import LightningModule, LightningDataModule
-from pytorch_lightning.cli import LightningCLI
+from pytorch_lightning.cli import LightningArgumentParser, LightningCLI
 from electrolyte_fm.utils.ckpt import SaveConfigWithCkpts
 import torch
 from torch.utils.data import DataLoader
 
 
 class MockedModel(LightningModule):
-    def __init__(self, vocab_size):
+    def __init__(self, vocab_size: int, linked: int):
         self.save_hyperparameters()
         super().__init__()
 
@@ -24,7 +26,7 @@ class MockedModel(LightningModule):
 
 
 class MockedData(LightningDataModule):
-    def __init__(self, tokenizer: str):
+    def __init__(self, tokenizer: str, linked: int):
         self.tokenizer = tokenizer
         self.save_hyperparameters()
         super().__init__()
@@ -35,21 +37,42 @@ class MockedData(LightningDataModule):
 
 @pytest.fixture()
 def cli(tmp_path):
-    return LightningCLI(
+    
+    with mock.patch("sys.argv", [ 
+        "any.py",
+        "--data.tokenizer=smirk",
+        "--data.linked=10", 
+        "--model.vocab_size=256"
+        ]):
+        parser = LightningArgumentParser()
+        parser.add_class_arguments(MockedModel, "model")
+        parser.add_class_arguments(MockedData, "data")
+        parser.link_arguments(
+            "data.linked", 
+            "model.linked",
+            apply_on="parse"
+            )
+        parsed_args = dict(parser.parse_args())
+        args_ = ["fit", ]
+        args_.extend([
+                "--" + k + "=" + str(v) for k, v in parsed_args.items()
+            ])
+
+    _cli =  LightningCLI(
         trainer_defaults={
             "max_steps": 2,
             "default_root_dir": tmp_path,
         },
-        model_class=MockedModel,
-        datamodule_class=MockedData,
+        model_class = MockedModel,
+        datamodule_class = MockedData,
         save_config_callback=SaveConfigWithCkpts,
-        args=["fit", "--data.tokenizer=smirk", "--model.vocab_size=256"],
-    )
+        args= args_
+        )
+    return _cli
 
 
 def test_ckpt(cli):
     # Locate callback
-    print(cli.trainer.callbacks)
     cb = list(
         filter(lambda cb: isinstance(cb, SaveConfigWithCkpts), cli.trainer.callbacks)
     )
@@ -62,7 +85,7 @@ def test_ckpt(cli):
     assert cb.config_path.joinpath("model_hparams.json").is_file()
 
     # Check that the dataloader config is saved
-    data_config = {"tokenizer": "smirk"}
+    data_config = {"linked": 10, "tokenizer": "smirk"}
     assert dict(cb.config["data"]) == data_config
     with open(cb.config_path.joinpath("config.json"), "r") as fid:
         assert json.load(fid)["data"] == data_config
@@ -71,5 +94,5 @@ def test_ckpt(cli):
     with open(cb.config_path.joinpath("model_hparams.json"), "r") as fid:
         model_config = json.load(fid)
     assert model_config["class_path"] == __name__ + ".MockedModel"
-    assert model_config["init_args"] == {"vocab_size": 256}
+    assert model_config["init_args"] == {"linked": 10, "vocab_size": 256}
     assert "version" in model_config.keys()
